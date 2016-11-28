d8f01238eac71800d080615894615ed9baebc14a
CirtinaFlorin <florin.cirtina@outlook.com>
2016-11-29 00:34:19 +0200

    Course-8

---
+++ Course8/Controllers/EvaluationController.cs
@@ -0,0 +1,103 @@
+﻿using Course8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Course8.Controllers
+{
+    public class EvaluationController : Controller
+    {
+        // GET: Evaluation
+        public ActionResult Index()
+        {
+            return View(ListOfProject);
+        }
+
+        // GET: Evaluation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Evaluation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Evaluation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Evaluation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        public static List<ProjectEvaluation> ListOfProject = new List<ProjectEvaluation>() {
+            new ProjectEvaluation() {
+                Name="Florin",
+                City="<script>alert('xss');</script>",
+                Country="Romania",
+                Rating=8
+                
+
+            }
+        };
+    }
+}
